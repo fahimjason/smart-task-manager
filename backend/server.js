a1f6c7ac9cdd14ac8de142c33751bd56cb57ed7d
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2025-11-18 16:58:27 +0600

    Setup initial backend for smart task manager project

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const colors = require('colors');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
+
+// Load env variables
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Connect to database
+connectDB();
+
+app.use(errorHandler);
+
+// Health Check
+app.get('/', (req, res) => {
+    res.json({ message: 'Smart Task Manager API is running' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server and exit process
+    server.close(process.exit(1));
+})
