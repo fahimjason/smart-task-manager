67cc3f4c99b392da4fa3e799d997bac7ae619910
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2025-11-19 19:22:15 +0600

    Add context

---
+++ task-manager-frontend/src/contexts/AuthContext.js
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Example: check token from localStorage
+        const token = localStorage.getItem("token");
+
+        setIsAuthenticated(!!token);
+        setLoading(false);
+    }, []);
+
+    const login = (token) => {
+        localStorage.setItem("token", token);
+        setIsAuthenticated(true);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setIsAuthenticated(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, loading, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
